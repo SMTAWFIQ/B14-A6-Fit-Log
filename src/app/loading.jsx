e65fe884aa7d2fe29e79d2470a6552ab8dee7e07
SMTAWFIQ <sktwfqb69@gmail.com>
2026-09-26 22:15:34 +0600

    Added a message in global loading

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,6 +4,7 @@ const GlobalLoading = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <span className="loading loading-spinner text-warning"></span>
+      Loading Homepage...
     </div>
   );
 };
